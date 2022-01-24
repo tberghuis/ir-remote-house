feb4f01f09622db7db59995fc7a1c0229610b506
Thomas Berghuis <thomas.berghuis@gmail.com>
2022-01-24 16:09:21 +1100

    .

--- app/src/main/java/xyz/tberghuis/irblaster/ui/vm/RemoteViewModel.kt
+++ app/src/main/java/xyz/tberghuis/irblaster/ui/vm/RemoteViewModel.kt
@@ -9,6 +9,7 @@ import javax.inject.Inject
 @HiltViewModel
 class RemoteViewModel
 @Inject constructor(
+  // SavedStateHandle get navigation path on init i.e. /my-tv-remote
   savedStateHandle: SavedStateHandle
 ) : ViewModel() {
   val helloRemoteViewModel = "remote view model"
