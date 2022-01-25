5633c5d887a2d7256c3255001120b57d985daca3
Thomas Berghuis <thomas.berghuis@gmail.com>
2022-01-25 11:26:53 +1100

    .

--- my-custom-remote/src/App.jsx
+++ my-custom-remote/src/App.jsx
@@ -1,12 +1,10 @@
-import { useState } from "react";
-import logo from "./logo.svg";
-
+// import { useState } from "react";
 import Button from "./components/Button";
 
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
+  // const [count, setCount] = useState(0);
 
   return (
     <div className="App">
