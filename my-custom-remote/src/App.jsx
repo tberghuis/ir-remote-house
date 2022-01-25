cfa835a45e8263b6e2984d41be9e99052ab6adad
Thomas Berghuis <thomas.berghuis@gmail.com>
2022-01-25 15:48:59 +1100

    .

--- my-custom-remote/src/App.jsx
+++ my-custom-remote/src/App.jsx
@@ -4,18 +4,37 @@ import * as patterns from "./patterns";
 import "./App.css";
 
 function App() {
-  // const [count, setCount] = useState(0);
-
   return (
     <div className="my-remote">
       <div>JVC TV upload</div>
       <br />
       <Button label="power" pattern={patterns.PTN_POWER} />
-      <br />
-      <br />
       <Button label="mute" pattern={patterns.PTN_MUTE} />
-      <br />
-      <div>todo...</div>
+
+      <Button label="back" pattern={patterns.PTN_BACK} />
+
+      <Button label="up" pattern={patterns.PTN_UP} />
+      <div className="row">
+        <Button label="left" pattern={patterns.PTN_LEFT} />
+        <Button label="ok" pattern={patterns.PTN_OK} />
+        <Button label="right" pattern={patterns.PTN_RIGHT} />
+      </div>
+      <Button label="down" pattern={patterns.PTN_DOWN} />
+
+      <Button label="vol+" pattern={patterns.PTN_VOLUP} />
+      <Button label="vol-" pattern={patterns.PTN_VOLDOWN} />
+
+      <Button label="source" pattern={patterns.PTN_SOURCE} />
+
+      <Button label="netflix" pattern={patterns.PTN_NETFLIX} />
+
+      <Button label="play" pattern={patterns.PTN_PLAY} />
+      <Button label="pause" pattern={patterns.PTN_PAUSE} />
+
+      <Button label="<<" pattern={patterns.PTN_REWIND} />
+      <Button label="|<<" pattern={patterns.PTN_SKIPPREV} />
+      <Button label=">>|" pattern={patterns.PTN_SKIPNEXT} />
+      <Button label=">>" pattern={patterns.PTN_FFWD} />
     </div>
   );
 }
--- my-custom-remote/src/patterns.js
+++ my-custom-remote/src/patterns.js
@@ -1,10 +1,6 @@
 // patterns for JVC TV remote
 
-// TODO migrate from string to int array
-export const PTN_POWER = "1,2,3,4";
-// export const PTN_MUTE =
-//   "8938,4479,533,533,575,533,575,554,575,554,575,554,575,554,575,1663,575,554,575,1663,575,1663,575,1663,575,1663,575,1663,575,1663,575,554,575,1663,575,554,575,554,575,554,575,554,575,1663,575,554,554,575,554,575,1663,575,1663,575,1663,575,1663,575,554,575,1663,575,1663,575,1663,575,39658,8938,2218,533,96298,8938,2218,533";
-
+export const PTN_POWER = [1, 2, 3, 4];
 export const PTN_MUTE = [
   8938, 4479, 533, 533, 575, 533, 575, 554, 575, 554, 575, 554, 575, 554, 575,
   1663, 575, 554, 575, 1663, 575, 1663, 575, 1663, 575, 1663, 575, 1663, 575,
@@ -18,9 +14,9 @@ export const PTN_BACK = [1, 2, 3, 4];
 
 export const PTN_UP = [1, 2, 3, 4];
 export const PTN_LEFT = [1, 2, 3, 4];
+export const PTN_OK = [1, 2, 3, 4];
 export const PTN_RIGHT = [1, 2, 3, 4];
 export const PTN_DOWN = [1, 2, 3, 4];
-export const PTN_OK = [1, 2, 3, 4];
 
 export const PTN_VOLUP = [1, 2, 3, 4];
 export const PTN_VOLDOWN = [1, 2, 3, 4];
