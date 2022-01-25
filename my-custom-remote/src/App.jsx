e2aafec0175821f7c3a3e256d248ae46e547f817
Thomas Berghuis <thomas.berghuis@gmail.com>
2022-01-26 07:21:53 +1100

    .

--- my-custom-remote/src/App.jsx
+++ my-custom-remote/src/App.jsx
@@ -5,36 +5,41 @@ import "./App.css";
 
 function App() {
   return (
-    <div className="my-remote">
-      <div className="font-bold">JVC TV upload</div>
+    <div className="my-remote-container">
+      <div className="">JVC TV</div>
       <br />
       <Button label="power" pattern={patterns.PTN_POWER} />
       <Button label="mute" pattern={patterns.PTN_MUTE} />
 
-      <Button className="margin" label="back" pattern={patterns.PTN_BACK} />
+      <Button className="" label="back" pattern={patterns.PTN_BACK} />
 
       <Button label="up" pattern={patterns.PTN_UP} />
-      <div className="row">
-        <Button label="left" pattern={patterns.PTN_LEFT} />
+      <div className="space-x-3">
+        <Button label="<-" pattern={patterns.PTN_LEFT} />
         <Button label="ok" pattern={patterns.PTN_OK} />
-        <Button label="right" pattern={patterns.PTN_RIGHT} />
+        <Button label="->" pattern={patterns.PTN_RIGHT} />
       </div>
       <Button label="down" pattern={patterns.PTN_DOWN} />
 
       <Button label="vol+" pattern={patterns.PTN_VOLUP} />
       <Button label="vol-" pattern={patterns.PTN_VOLDOWN} />
 
-      <Button label="source" pattern={patterns.PTN_SOURCE} />
+      <div className="space-x-3">
+        <Button label="source" pattern={patterns.PTN_SOURCE} />
+        <Button label="netflix" pattern={patterns.PTN_NETFLIX} />
+      </div>
 
-      <Button label="netflix" pattern={patterns.PTN_NETFLIX} />
+      <div className="space-x-3">
+        <Button label="play" pattern={patterns.PTN_PLAY} />
+        <Button label="pause" pattern={patterns.PTN_PAUSE} />
+      </div>
 
-      <Button label="play" pattern={patterns.PTN_PLAY} />
-      <Button label="pause" pattern={patterns.PTN_PAUSE} />
-
-      <Button label="<<" pattern={patterns.PTN_REWIND} />
-      <Button label="|<<" pattern={patterns.PTN_SKIPPREV} />
-      <Button label=">>|" pattern={patterns.PTN_SKIPNEXT} />
-      <Button label=">>" pattern={patterns.PTN_FFWD} />
+      <div className="space-x-3">
+        <Button label="<<" pattern={patterns.PTN_REWIND} />
+        <Button label="|<<" pattern={patterns.PTN_SKIPPREV} />
+        <Button label=">>|" pattern={patterns.PTN_SKIPNEXT} />
+        <Button label=">>" pattern={patterns.PTN_FFWD} />
+      </div>
     </div>
   );
 }
